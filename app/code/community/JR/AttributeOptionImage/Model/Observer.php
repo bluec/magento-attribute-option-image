0bd47b4c2becbf0c0b6d88b9a010edbb091124d2
Mathis Klooß <mathis@mage-profis.de>
2016-11-25 08:42:16 +0100

    fixed uploader issue SUPEE-8788

--- app/code/community/JR/AttributeOptionImage/Model/Observer.php
+++ app/code/community/JR/AttributeOptionImage/Model/Observer.php
@@ -9,12 +9,30 @@ class JR_AttributeOptionImage_Model_Observer
             ->setCanLoadExtJs(true)
             ->addJs('mage/adminhtml/variables.js')
             ->addJs('mage/adminhtml/wysiwyg/widget.js')
-            ->addJs('lib/flex.js')
-            ->addJs('lib/FABridge.js')
-            ->addJs('mage/adminhtml/flexuploader.js')
+            ->addItem('js_css', 'prototype/windows/themes/default.css')
+        ;
+        // Patch SUPEE-8788
+        if (file_exists(Mage::getBaseDir('base').DS.'js'.DS.'lib'.DS.'uploader'.DS.'flow.min.js'))
+        {
+            $layout->getBlock('head')
+                ->addJs('lib/uploader/flow.min.js')
+                ->addJs('lib/uploader/fusty-flow.js')
+                ->addJs('lib/uploader/fusty-flow-factory.js')
+                ->addJs('mage/adminhtml/uploader/instance.js')
+            ->addItem('skin_css', 'lib/prototype/windows/themes/magento.css')
+            ;
+        } else {
+            // No Patch SUPEE-8788
+            $layout->getBlock('head')
+                ->addJs('lib/flex.js')
+                ->addJs('lib/FABridge.js')
+                ->addJs('mage/adminhtml/flexuploader.js')
+                ->addItem('js_css', 'prototype/windows/themes/magento.css')
+            ;
+        }
+        $layout->getBlock('head')
             ->addJs('mage/adminhtml/browser.js')
             ->addJs('prototype/window.js')
-            ->addItem('js_css', 'prototype/windows/themes/default.css')
-            ->addItem('js_css', 'prototype/windows/themes/magento.css');
+        ;
     }
 }
